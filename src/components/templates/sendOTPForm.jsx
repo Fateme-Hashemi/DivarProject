99494a37de1dc25ff07515e387521e896b9b62c0
elahe hs <elahe.hs481@gmail.com>
2024-02-25 15:47:44 +0330

    sendOtp func

--- src/components/templates/sendOTPForm.jsx
+++ src/components/templates/sendOTPForm.jsx
@@ -1,7 +1,46 @@
-const SendOPTForm = () => {
-    return ( <div>
-        SendOPTForm
-    </div> );
+
+import PropTypes from 'prop-types';
+import { SendOtp } from '../../services/auth';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+function SendOPTForm ({setStep, mobile, setMobile}){
+    const submitHandler = async (e)=> {
+        e.preventDefault();
+        console.log(e);
+        if(mobile.length !== 11) return;
+        const {response, error} =  await SendOtp(mobile);
+     
+            if(response) {
+                setStep(2);
+                toast.success("کد تایید با موفقیت ارسال شد.");
+            }
+        if(error) {
+                toast.error(error.response.data.message);
+        }
+    }
+    return ( 
+        <div>
+         
+   <form onSubmit={submitHandler}>
+            <p>ورود به حساب کاربری</p>
+            <span>
+                برای استفاده از امکانات دیوار، لطفا شماره موبایل خود را وارد کنید. کد تایید به این شماره پیامک  خواهد شد.
+            </span>
+            <label htmlFor="input">
+شماره موبایل خود را وارد کنید.
+            </label>
+            <input type="text" id="input" placeholder="شماره موبایل" value={mobile} onChange={(e) => setMobile(e.target.value)} />
+            <button type="submit">ارسال کد تایید</button>
+        </form>
+        <ToastContainer />
+        </div>
+     
+     );
 }
- 
+SendOPTForm.propTypes = {
+    setStep: PropTypes.func.isRequired,
+    mobile: PropTypes.string.isRequired,
+    setMobile: PropTypes.func.isRequired
+};
 export default SendOPTForm;
--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -2,14 +2,17 @@ import { useState } from "react";
 import CheckOTPForm from "../components/templates/checkOTPForm";
 import SendOPTForm from "../components/templates/sendOTPForm";
 
-const AuthPage = () => {
+
+
+
+function AuthPage() {
     const [step, setStep] = useState(1);
     const [mobile, setMobile] = useState('');
     const [code, setCode]= useState('');
 
     return (
         <div>
-        {step === 1 && <SendOPTForm /> }
+        {step === 1 && <SendOPTForm setStep={setStep} mobile={mobile} setMobile={setMobile} /> }
         {step === 2  && <CheckOTPForm /> }
         </div>
     )
