fde6561af5da1865ad8f0ffa3bca16e59f87a158
elahe hs <elahe.hs481@gmail.com>
2024-02-27 16:57:17 +0330

    add react query

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import { BrowserRouter } from "react-router-dom";
 import Router from "./router/Router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import defaultOptions from "configs/reactQuery";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 
 function App() {
+  const queryClient = new QueryClient({defaultOptions});
   return (
-    <div>
-        <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
        <Router />
           </BrowserRouter>
-    </div>
+          <ReactQueryDevtools />
+      </QueryClientProvider>
   );
 }
 
