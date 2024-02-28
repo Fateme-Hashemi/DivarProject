6fa3c0a947dd05b69acd9ffd892a48d8f3c9199b
elahe hs <elahe.hs481@gmail.com>
2024-02-28 13:42:10 +0330

    add getProfile

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -4,8 +4,15 @@ import HomePage from "pages/HomePage";
 import AdminPage from "pages/AdminPage";
 import Dashboard from "pages/Dashboard";
 import NotFound from "pages/404";
+import { useQuery } from "@tanstack/react-query";
+import { getProfile } from "services/user";
 
 function Router() {
+    const {data, isLoading, error} = useQuery(['profile'], getProfile);
+    console.log(data, isLoading, error);
+    if(isLoading) {
+        return <h1>Loading ...</h1>
+    }
     return (
         <Routes>
         <Route path="/" element={<HomePage />} />
--- src/utils/cookie.js
+++ src/utils/cookie.js
@@ -3,5 +3,7 @@ const SetCookie = token => {
 
     document.cookie = `refreshToken=${token.refreshToken}; max-age=${30*24*60*60}`;
 }
-
-export {SetCookie};
+const getCookie = (cookieName) => {
+  return document.cookie.split(";").find((token)=> token.trim().split("=")[0] === cookieName)?.split("=")[1];
+}
+export {SetCookie, getCookie};
