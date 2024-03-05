7f7fd55fc55edae4c7f68d5472d4717fdbd9487f
elahe hs <elahe.hs481@gmail.com>
2024-03-05 11:56:17 +0330

    add layouts

---
+++ src/layouts/Footer.jsx
@@ -0,0 +1,11 @@
+import styles from './styles.module.css'
+
+function Footer() {
+    return (
+        <footer className={styles.footer}>
+            <p>Developed by Elahe with </p> <span> &hearts;</span>
+        </footer>
+    )
+}
+
+export default Footer
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const paths = ['src', 'assets', 'components', 'configs', 'pages', 'router', 'services', 'styles', 'utils'];
+const paths = ['src', 'assets', 'components', 'configs', 'pages', 'router', 'services', 'styles', 'utils', 'layouts'];
 
 
 
